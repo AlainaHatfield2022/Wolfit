2c969afb4dceb38fed50c4ecbd34eca0359b52d7
Alaina Hatfield <ahatfield20@mail.wou.edu>
2022-01-22 10:32:24 -0800

    seconds ago now running

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -2,7 +2,8 @@ from app.helpers import pretty_date, less_than_day
 from datetime import datetime, timedelta
 def test_now():
     assert (pretty_date(datetime.utcnow())) == "just now"
-
+def seconds_ago():
+    assert pretty_date(datetime.utcnow() - timedelta(seconds=60)) == "60 seconds ago"
 
 def test_Yesterday():
     assert pretty_date(datetime.utcnow() - timedelta(days=1)) == "Yesterday"
