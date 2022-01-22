de65311e4a4467ed2af0bed2819040507ed0e5ac
Alaina Hatfield <ahatfield20@mail.wou.edu>
2022-01-22 10:21:21 -0800

    first tests are running with testnow and yesterday weeks ago and days ago and months ago

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -10,3 +10,5 @@ def test_days_ago():
     assert pretty_date(datetime.utcnow() - timedelta(days=6)) == "6 days ago"
 def test_weeks_ago():
     assert pretty_date(datetime.utcnow() - timedelta(weeks=2)) == "2 weeks ago"
+def test_months_ago():
+    assert pretty_date(datetime.utcnow() - timedelta(days=60)) == "2 months ago"
