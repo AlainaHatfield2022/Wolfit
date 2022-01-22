87a747d9af5a3162a4e0bb664187875415acdb89
Alaina Hatfield <ahatfield20@mail.wou.edu>
2022-01-22 09:59:18 -0800

    first tests are running with testnow and yesterday but having issues with seconds ago

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -1,8 +1,8 @@
-from app.helpers import pretty_date
+from app.helpers import pretty_date, less_than_day
 from datetime import datetime, timedelta
 def test_now():
     assert (pretty_date(datetime.utcnow())) == "just now"
-def seconds_ago():
-    assert (pretty_date(datetime.utcnow() - timedelta(seconds=60))) == "seconds ago"
-def test_yesterday():
-    assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
+
+
+def test_Yesterday():
+    assert pretty_date(datetime.utcnow() - timedelta(days=1)) == "Yesterday"
