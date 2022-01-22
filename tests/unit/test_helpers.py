cdbde2e7054e8f1a35ad2a5230f0aa6dab8c0007
Alaina Hatfield <ahatfield20@mail.wou.edu>
2022-01-22 10:45:02 -0800

    seconds ago running but now showing up on coverage

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -2,8 +2,8 @@ from app.helpers import pretty_date, less_than_day
 from datetime import datetime, timedelta
 def test_now():
     assert (pretty_date(datetime.utcnow())) == "just now"
-def seconds_ago():
-    assert less_than_day(datetime.utcnow() - timedelta(seconds=59)) == "59 seconds ago"
+def test_seconds_ago():
+    assert pretty_date(datetime.utcnow() - timedelta(seconds=30)) == "30 seconds ago"
 def test_Yesterday():
     assert pretty_date(datetime.utcnow() - timedelta(days=1)) == "Yesterday"
 def test_days_ago():
