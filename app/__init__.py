97acdd7edb0d9474ab48c340b40325a99e7966a1
Alaina Hatfield <ahatfield20@mail.wou.edu>
2022-03-17 10:11:35 -0700

    new updates to make wolfit run

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 app = Flask(__name__)
 app.config.from_object(config.Config)
 app.config.from_envvar('WOLFIT_SETTINGS')
